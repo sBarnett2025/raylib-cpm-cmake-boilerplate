bddf83d802ea8827f0e4d824ddcb848d3553581a
Samuel Barnett <samuel.barnett@mymail.champlain.edu>
2024-01-25 14:15:27 -0500

    1/25 class work, added noise

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <raylib.h>
 #include <cstdint>
+#include "FastNoiseLite.h"
 
 int main()
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-    const int imgWidth = 400;
-    const int imgHeight = 400;
+    const int screenWidth = 1000;
+    const int screenHeight = 1000;
+    const int imgWidth = 800;
+    const int imgHeight = 800;
 
     InitWindow(screenWidth, screenHeight, "Pixel Manipulation");
 
@@ -17,18 +18,50 @@ int main()
     auto pixels = (Color*)img.data;
     Texture tex = LoadTextureFromImage(img);
 
+    FastNoiseLite base;
+    base.SetFractalOctaves(3);
+    base.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2S);
+    base.SetFractalType(FastNoiseLite::FractalType_FBm);
+    //FastNoiseLite cellular;
+    //cellular.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
+    //cellular.SetFractalOctaves(3);
+
+
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+
         // your update logic goes here
-        for(int row = 0; row < imgHeight; row++)
+        for(int y = 0; y < imgHeight; y++)
         {
-            for(int col = 0; col < imgWidth; col++)
+            for(int x = 0; x < imgWidth; x++)
             {
-                pixels[row * imgWidth + col] = (Color) {
-                    (uint8_t)(GetRandomValue(0, 255)),
-                    (uint8_t)(GetRandomValue(0, 255)),
-                    (uint8_t)(GetRandomValue(0, 255)),
+                float posY = (float)((y - imgHeight / 2)) / ((float)imgHeight / 2);
+                float posX = (float)((x - imgHeight / 2)) / ((float)imgHeight / 2);
+
+                float islandInfluence = (2 - (abs(posX) + abs(posY))) / 2;
+                islandInfluence *= 255;
+                float displacement = 0;
+                float c1 = ((base.GetNoise((float)x, (float)y, displacement * 50) + 1) / 2) * 255;
+
+                auto avg = (c1 + islandInfluence) / 2;
+
+                if (avg < 50)
+                    pixels[y * imgWidth + x] = (Color){0,0,139,255}; // Dark Blue
+                else if (avg < 100)
+                    pixels[y * imgWidth + x] = (Color){255,255,153,255}; // Yellow
+                else if (avg < 150)
+                    pixels[y * imgWidth + x] = (Color){0,255,0,255}; // Green
+                else if (avg < 200)
+                    pixels[y * imgWidth + x] = (Color){165,42,0,255}; // Brown
+                else
+                    pixels[y * imgWidth + x] = (Color){255,255,255,255}; // White
+
+
+                pixels[y * imgWidth + x] = (Color){
+                    (uint8_t)avg,
+                    (uint8_t)avg,
+                    (uint8_t)avg,
                     255
                 };
             }
@@ -38,7 +71,7 @@ int main()
         // drawing logic goes here
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawTexture(tex, 200, 25, WHITE);
+        DrawTexture(tex, 100, 100, WHITE);
         EndDrawing();
     }
 
