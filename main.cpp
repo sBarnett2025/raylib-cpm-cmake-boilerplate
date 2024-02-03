db90d7d8d8fc4fb5ba7e3c706d7a2c4b69ab31be
Samuel Barnett <samuel.barnett@mymail.champlain.edu>
2024-02-03 14:32:55 -0500

    working on hydraulic erosion

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <raylib.h>
 #include <cstdint>
 #include "FastNoiseLite.h"
+#include "iostream"
 
 int main()
 {
     const int screenWidth = 1000;
     const int screenHeight = 1000;
-    const int imgWidth = 800;
-    const int imgHeight = 800;
+    const int imgWidth = 400;
+    const int imgHeight = 400;
 
     InitWindow(screenWidth, screenHeight, "Pixel Manipulation");
 
@@ -15,6 +16,7 @@ int main()
 
     Image img = GenImageColor(imgHeight, imgHeight, WHITE);
     ImageFormat(&img, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
+    float *heightData = new float(imgWidth * imgHeight);
     auto pixels = (Color*)img.data;
     Texture tex = LoadTextureFromImage(img);
 
@@ -23,59 +25,88 @@ int main()
     base.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2S);
     base.SetFractalType(FastNoiseLite::FractalType_FBm);
 
+    int counter = 0;
 
+    for(int y = 0; y < imgHeight; y++)
+    {
+        for(int x = 0; x < imgWidth; x++)
+        {
+            float posY = (float)((y - imgHeight / 2)) / ((float)imgHeight / 2);
+            float posX = (float)((x - imgWidth / 2)) / ((float)imgWidth / 2);
+
+            float islandInfluence = (2 - (abs(posX) + abs(posY))) / 2;
+            islandInfluence *= 255;
+            //float displacement = 100;
+            float c1 = ((base.GetNoise((float)x, (float)y, 0.0f) + 1) / 2) * 255;
+            counter++;
+            //std::cout << counter << std::endl;
+            //std::cout << "x: " << x << " y: " << y << std::endl;
+            //std::cout << c1 << std::endl;
+
+            auto avg = (c1 + islandInfluence) / 2;
+
+            heightData[y * imgWidth + x] = avg;
+
+        }
+    }
+
+    counter = 0;
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-
         // your update logic goes here
+
+        //std::cout << "ggg" << std::endl;
         for(int y = 0; y < imgHeight; y++)
         {
             for(int x = 0; x < imgWidth; x++)
             {
-                float posY = (float)((y - imgHeight / 2)) / ((float)imgHeight / 2);
-                float posX = (float)((x - imgHeight / 2)) / ((float)imgHeight / 2);
-
-                float islandInfluence = (2 - (abs(posX) + abs(posY))) / 2;
-                islandInfluence *= 255;
-                float displacement = 100;
-                float c1 = ((base.GetNoise((float)x, (float)y, 0.0f) + 1) / 2) * 255;
-
-                auto avg = (c1 + islandInfluence) / 2;
-
-                if (avg < 190)
+                counter++;
+                //heightData[y * imgWidth + x] -= 25;
+                //std::cout << counter << std::endl;
+                //std::cout << "x: " << x << " y: " << y << std::endl;
+                if (heightData[y * imgWidth + x] < 190)
+                {
                     pixels[y * imgWidth + x] = (Color){0,0,139,255}; // Dark Blue
-                else if (avg < 200)
-                    pixels[y * imgWidth + x] = (Color){255,255,153,255}; // Yellow
-                else if (avg < 210)
+                }
+                else if (heightData[y * imgWidth + x] < 200)
+                {
+                    pixels[y * imgWidth + x] = (Color) {255, 255, 153, 255}; // Yellow
+                }
+                else if (heightData[y * imgWidth + x] < 210)
+                {
                     pixels[y * imgWidth + x] = (Color){0,139,0,255}; // Green
-                else if (avg < 220)
+                }
+                else if (heightData[y * imgWidth + x] < 220)
+                {
                     pixels[y * imgWidth + x] = (Color){0,100,0,255}; // Dark Green
-                else if (avg < 230)
+                }
+                else if (heightData[y * imgWidth + x] < 230)
+                {
                     pixels[y * imgWidth + x] = (Color){165,42,0,255}; // Brown
+                }
                 else
+                {
                     pixels[y * imgWidth + x] = (Color){255,255,255,255}; // White
-
-
-                    /*
-                pixels[y * imgWidth + x] = (Color){
-                    (uint8_t)avg,
-                    (uint8_t)avg,
-                    (uint8_t)avg,
-                    255
-                };
-                */
-
+                }
             }
         }
+
+
+
+
         UpdateTexture(tex, pixels);
 
         // drawing logic goes here
         BeginDrawing();
         ClearBackground(BLACK);
         DrawTexture(tex, 100, 100, WHITE);
+        std::cout << "dsadasdsadasdas" << std::endl;
         EndDrawing();
+
     }
+
+    delete heightData;
 
     UnloadTexture(tex);
 
